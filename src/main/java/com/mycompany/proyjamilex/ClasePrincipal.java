97e729b59d9ec62a96bcf3fdcc7cf141458df8ee
JAMILEX <Jamilexgarcia2002@gmail.com>
2023-10-29 22:46:36 -0500

    Commit 4 alternativas

--- src/main/java/com/mycompany/proyjamilex/ClasePrincipal.java
+++ src/main/java/com/mycompany/proyjamilex/ClasePrincipal.java
@@ -11,7 +11,24 @@ public class ClasePrincipal {
         GeneradorProblemas generador = new GeneradorProblemas(nivel, tipo);
         RastreadorRespuestas rastreador = new RastreadorRespuestas();
 
+        int totalPreguntas = 5;
+        int intentos = 0;
 
+        while (intentos < totalPreguntas) {
+            int[] operandos = generador.generarOperandos();
+            System.out.println("Cuánto es " + operandos[0] + " + " + operandos[1] + "?");
+            int respuesta = scanner.nextInt();
+
+            if (respuesta == operandos[0] + operandos[1]) {
+                rastreador.respuestaCorrecta();
+                rastreador.generarMensajePositivo();
+            } else {
+                rastreador.respuestaIncorrecta();
+                rastreador.generarMensajeNegativo();
+            }
+
+            intentos++;
+        }
 
         scanner.close();
     }
